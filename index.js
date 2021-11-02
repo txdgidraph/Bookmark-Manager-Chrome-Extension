70e61ffaac48d1e2e1675ac47e1388f23aab432a
phinix <txdgidraph>
2021-11-03 01:00:06 +0300

    Leverage Local storage & Improve Time Complexity

--- index.js
+++ index.js
@@ -2,24 +2,33 @@ let myLead = [];
 const inputEl = document.getElementById("input-el");
 const inputBtn = document.getElementById("input-btn");
 const ulEl = document.getElementById("ul-el");
-
+//localStorage.clear()
 inputBtn.addEventListener("click", function () {
   myLead.push(inputEl.value);
+  localStorage.setItem("myLeads", JSON.stringify(myLead));
   renderLeads();
-  myLead.pop();
+  //myLead.pop();
   inputEl.value = null;
 });
 
 function renderLeads() {
-  let listItems = "";
-  for (let item = 0; item < myLead.length; item++) {
-    listItems = myLead[item];
-    const li = document.createElement("li");
-    const a = document.createElement("a");
-    a.textContent += listItems;
-    li.append(a);
-    a.href = myLead[item];
-    a.target = "_blank"
-    ulEl.append(li);
-  }
+  // let listItems = "";
+  // for (let item = 0; item < myLead.length; item++) {
+  //   listItems = myLead[item];
+  //   const li = document.createElement("li");
+  //   const a = document.createElement("a");
+  //   a.textContent += listItems;
+  //   li.append(a);
+  //   a.href = myLead[item];
+  //   a.target = "_blank"
+  //   ulEl.append(li);
+  //   myLead.pop();
+  // }
+  let lastItem = myLead[myLead.length - 1];
+  const li = document.createElement("li");
+  const a = document.createElement("a");
+  a.href = lastItem;
+  a.textContent += lastItem;
+  li.append(a);
+  ulEl.append(li);
 }
